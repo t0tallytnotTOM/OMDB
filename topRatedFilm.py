1447e93e85f24d0d6eb682461dd58bb03e0dbc17
t0tallytnotTOM <37673027+t0tallytnotTOM@users.noreply.github.com>
2018-03-26 14:06:37 +0100

    Top rated film
    function for the top rated film

---
+++ topRatedFilm.py
@@ -0,0 +1,24 @@
+def topRated():
+    with open("films.txt") as filmsFile:
+        highDb = {}
+        for film in filmsFile:
+            c = 0
+            film = film.split(",")
+            ratings = film[11]
+            name = film[0]
+            ratings = ratings.split(":")
+            for rate in ratings:
+                c += float(rate)
+            avg = c / len(ratings)
+            highDb[name] = avg
+        maxi = max(highDb, key=highDb.get)
+        return maxi + ": " + str(round(highDb[maxi],1))
+
+print(topRated())
+
+# I should say that for this to work, in films.txt, after the last real film
+# listing, there should be no more lines i.e. if there are 5 film listings
+# then there should only be 5 lines in the file.
+
+# The idea is that when you login, or go to the home page, or somewhere that
+# you can view the top rated film, that this function can be called    :)
