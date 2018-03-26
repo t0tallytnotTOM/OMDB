0c4cad55c4d5c802b6bec25c59e7c71fde7cd1c6
t0tallytnotTOM <37673027+t0tallytnotTOM@users.noreply.github.com>
2018-03-26 14:12:07 +0100

    Update topRatedFilm.py

--- topRatedFilm.py
+++ topRatedFilm.py
@@ -18,7 +18,9 @@ print(topRated())
 
 # I should say that for this to work, in films.txt, after the last real film
 # listing, there should be no more lines i.e. if there are 5 film listings
-# then there should only be 5 lines in the file.
+# then there should only be 5 lines in the file. Also, for a similar reason,
+# at the end of each set of ratings, there should be nothing (no commas or 
+# colons)
 
 # The idea is that when you login, or go to the home page, or somewhere that
 # you can view the top rated film, that this function can be called    :)
